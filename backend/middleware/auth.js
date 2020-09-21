89da0d854b6ce7a8ca8727003de256d18f7d4a06
Marion Touin <t.marion@hotmail.com>
2020-09-21 10:48:59 +0200

    like dislike

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,13 +8,13 @@ module.exports = (req, res, next) => {
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // Décoder le token
     const userId = decodedToken.userId; // Récupération du userID
     if (req.body.userId && req.body.userId !== userId) { // Vérifier si userID correspond au token
-      throw 'Invalid user ID';
+      throw 'l\'utilisateur n\'es pas valable';
     } else {
       next(); 
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: new Error('La requête n\'est pas valide!')
     });
   }
 };
